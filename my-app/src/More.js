15cd98913028255b487425d459c5fdc6d1df2ca9
Kecheng Sun <kecheng@D-10-19-217-140.dhcp4.washington.edu>
2018-11-15 22:20:50 -0800

    add new gh-pages

--- my-app/src/More.js
+++ my-app/src/More.js
@@ -6,6 +6,7 @@ export class More extends Component {
     render() {
         let social="https://www.instagram.com/_kcsun__/"
         let work="www.linkedin.com/in/kechengsun"
+        
         return (
         <div className="info">
             <h1>More info</h1>
