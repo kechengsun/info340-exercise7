20fed8c4a797237b077661c031d08f3d2ed7ba38
Kecheng Sun <kecheng@D-10-19-217-140.dhcp4.washington.edu>
2018-11-16 11:24:02 -0800

    Updates

---
+++ precache-manifest.590c871788d370ae2539351faeb5eab1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ffbabe3710b3bc93bacd",
+    "url": "/kechengsun/info340-exercise7/static/js/runtime~main.ffbabe37.js"
+  },
+  {
+    "revision": "de59bd1dd71b3b04d32e",
+    "url": "/kechengsun/info340-exercise7/static/js/main.de59bd1d.chunk.js"
+  },
+  {
+    "revision": "54b3ccbdffb767609d39",
+    "url": "/kechengsun/info340-exercise7/static/js/1.54b3ccbd.chunk.js"
+  },
+  {
+    "revision": "de59bd1dd71b3b04d32e",
+    "url": "/kechengsun/info340-exercise7/static/css/main.86c450bf.chunk.css"
+  },
+  {
+    "revision": "ff08bbf3504413ddc8681c1f756c32a1",
+    "url": "/kechengsun/info340-exercise7/index.html"
+  }
+];
